5810056fea19963f775620ad1ec3e46dd2c1d73c
MichaelHein65 <michael.hein.add@gmail.com>
2026-01-17 13:54:39 +0100

    Initial Commit

---
+++ Webradio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Webradio
+//
+//  Created by Michael Hein on 17.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Webradio/WebradioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebradioApp.swift
+//  Webradio
+//
+//  Created by Michael Hein on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct WebradioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
